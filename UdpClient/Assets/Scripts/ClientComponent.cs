e2e857fc4844161d8f70cfd596a35622d52549b8
Betty <827941041@qq.com>
2024-08-12 09:47:53 -0400

    Add sample UDP client & server

---
+++ UdpClient/Assets/Scripts/ClientComponent.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class ClientComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+        socket.Bind(ipPoint);
+
+        IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+        socket.SendTo(Encoding.UTF8.GetBytes("CLIENT IS HERE"),remoteIpPoint);
+        
+        byte[] bytes = new byte[512];
+        EndPoint remoteIpPoint2 = new IPEndPoint(IPAddress.Any, 0);
+        int length = socket.ReceiveFrom(bytes, ref remoteIpPoint2);
+        
+        print("MESSAGE RECEIVE " + Encoding.UTF8.GetString(bytes,0,length));
+        
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+}
---
+++ UdpServer/UdpServer/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+socket.Bind(ipPoint);
+Console.WriteLine("SERVER OKAY");
+
+// Receive
+byte[] bytes = new byte[512];
+EndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+int length = socket.ReceiveFrom(bytes, ref remoteIpPoint);
+Console.WriteLine("MESSAGE RECEIVE " + Encoding.UTF8.GetString(bytes, 0, length));
+
+// Send
+socket.SendTo(Encoding.UTF8.GetBytes("WELCOME TO THE SERVER"), remoteIpPoint);
+
+socket.Shutdown(SocketShutdown.Both);
+socket.Close();
+
+Console.ReadKey();
